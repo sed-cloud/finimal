d5597da840a777961637fd87fbed61f27d51be1d
Stephen Davis <stephenedavis17@gmail.com>
2022-02-06 12:03:43 -0500

    Updated secrets

--- pages/api/plaid/exchange_public_token/[publicToken].ts
+++ pages/api/plaid/exchange_public_token/[publicToken].ts
@@ -2,14 +2,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { Configuration, PlaidApi, PlaidEnvironments } from 'plaid';
 
 if (!process.env.FINIMAL_TIER) { throw new Error('Unable to load Plaid tier') }
-if (!process.env[`PLAID_CLIENT_ID_${process.env.FINIMAL_TIER}`]) { throw new Error('Unable to load Plaid client id') }
+if (!process.env[`PLAID_CLIENT_ID`]) { throw new Error('Unable to load Plaid client id') }
 if (!process.env[`PLAID_SECRET_${process.env.FINIMAL_TIER}`]) { throw new Error('Unable to load Plaid secret') }
 
 const configuration = new Configuration({
     basePath: PlaidEnvironments[process.env.FINIMAL_TIER.toLowerCase()],
     baseOptions: {
         headers: {
-            'PLAID-CLIENT-ID': process.env[`PLAID_CLIENT_ID_${process.env.FINIMAL_TIER}`],
+            'PLAID-CLIENT-ID': process.env[`PLAID_CLIENT_ID`],
             'PLAID-SECRET': process.env[`PLAID_SECRET_${process.env.FINIMAL_TIER}`],
         },
     },
